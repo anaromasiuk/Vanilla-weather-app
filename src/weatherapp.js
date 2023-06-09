4732b9b24b8bc141b9becdba142ca9dc517f37f8
anaromasiuk <teslik.anastasia@gmail.com>
2023-06-09 17:08:15 +0200

    time is updated

--- src/weatherapp.js
+++ src/weatherapp.js
@@ -1,3 +1,44 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let weekday = weekdays[date.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+
+  let day = date.getDate();
+
+  return `${weekday}, ${month} ${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector(".number");
@@ -5,11 +46,13 @@ function displayTemperature(response) {
   let feelslikeElement = document.querySelector("#feelslike");
   let humidityElement = document.querySelector("#humidity");
   let windspeedElement = document.querySelector("#windspeed");
+  let dateElement = document.querySelector(".date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   humidityElement.innerHTML = response.data.main.humidity + "%";
   feelslikeElement.innerHTML = Math.round(response.data.main.feels_like) + "°C";
   windspeedElement.innerHTML = response.data.wind.speed + "km/h";
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "f47a3ec592b9d7c74815dad665355000";
