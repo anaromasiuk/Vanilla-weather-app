96b760ce68e8b7b9362bbd043bd23d7765484f91
anaromasiuk <teslik.anastasia@gmail.com>
2023-06-09 18:03:23 +0200

    added icon image

--- src/weatherapp.js
+++ src/weatherapp.js
@@ -46,15 +46,22 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windspeedElement = document.querySelector("#windspeed");
   let dateElement = document.querySelector(".date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.name;
+  cityElement.innerHTML = "📍 " + response.data.name;
   humidityElement.innerHTML = response.data.main.humidity + "%";
   feelslikeElement.innerHTML = Math.round(response.data.main.feels_like) + "°C";
   windspeedElement.innerHTML = response.data.wind.speed + "km/h";
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 let apiKey = "f47a3ec592b9d7c74815dad665355000";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}&units=metric`;
+let city = "Krakow";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
