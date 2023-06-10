31a84f660dfde3d0c9f304b330edd2c4be719ce2
anaromasiuk <teslik.anastasia@gmail.com>
2023-06-10 22:39:58 +0200

    added the search engine

--- src/weatherapp.js
+++ src/weatherapp.js
@@ -60,8 +60,17 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "f47a3ec592b9d7c74815dad665355000";
-let city = "Krakow";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "f47a3ec592b9d7c74815dad665355000";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
